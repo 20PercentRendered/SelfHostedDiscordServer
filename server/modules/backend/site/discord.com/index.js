703b5d7fea74a075c196b2a610ab02f1f1f1a3fa
Onni Kukkonen <onni.kukkonen77@gmail.com>
2020-10-21 22:35:38 +0300

    add app routes

--- server/modules/backend/site/discord.com/index.js
+++ server/modules/backend/site/discord.com/index.js
@@ -5,6 +5,15 @@ const fs = require('fs')
 const app = require('express').Router();
 const passport = require('passport');
 const BearerStrategy = require('passport-http-bearer');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const routerMiddleware = createProxyMiddleware(
+    { target: 'https://162.159.130.233', changeOrigin: true,
+    headers: {
+      'Host': 'discord.com' //cloudflare will let us go if we have a Host header
+    }, 
+    secure: false
+});
+
 passport.use(new BearerStrategy(
     function(token, done) {
       User.findOne({ token: token }, function (err, user) {
@@ -14,6 +23,11 @@ passport.use(new BearerStrategy(
       });
     }
 ));
-
+app.use('/app', routerMiddleware);
+app.use('/api/updates', function (req, res) {
+  res.json({});
+});
+app.use('/api/modules', routerMiddleware);
+app.use('/assets', routerMiddleware);
 
 module.exports = app;
