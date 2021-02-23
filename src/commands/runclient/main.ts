29e785f7c50cc306968d714aee94fd00cbfee4c3
Onni Kukkonen <onni.kukkonen77@gmail.com>
2021-02-23 22:00:49 +0200

    command changes that i forgot in the last commit

---
+++ src/commands/runclient/main.ts
@@ -0,0 +1,48 @@
+import { Command } from "@main/classes/Command";
+import { Logger } from "@main/logger";
+import { ServerData } from "@main/serverdata";
+
+const os = require("os"),
+	path = require("path"),
+	fs = require("fs"),
+	logger = new Logger("RunClientCommand"),
+	port = ServerData.getInstance().settings.port,
+	child_process = require("child_process");
+export default class RunClientCommand extends Command {
+	public readonly name: string = "RunClient";
+	private args: Array<string>;
+	run(args?: any) {
+		var discordpath;
+		logger.info("Starting " + os.platform() + " client.");
+		switch (os.platform()) {
+			case "linux":
+				discordpath = "discord";
+				break;
+			case "win32":
+				var base = process.env.LOCALAPPDATA + "\\Discord";
+				var newestdiscord = null;
+				fs.readdirSync(base)
+					.sort()
+					.forEach((value) => {
+						if (newestdiscord != null) return;
+						if (value.includes("app-")) {
+							newestdiscord = value;
+						}
+					});
+				discordpath = path.resolve(`${base}\\${newestdiscord}\\Discord.exe`);
+				break;
+		}
+		child_process.exec(
+			discordpath + " " + this.args.toString().replace(/,/g, " ")
+		);
+	}
+	constructor() {
+		super();
+		this.args = [
+			`--host-rules="MAP * 127.0.0.1:${port}"`,
+			"--ignore-certificate-errors",
+			"--allow-insecure-localhost",
+			"--disable-http-cache",
+		];
+	}
+}
