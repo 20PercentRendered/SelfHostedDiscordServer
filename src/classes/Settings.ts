a0cf0c0bf418a739de0cd57dcc68fca5655b4d10
Onni Kukkonen <onni.kukkonen77@gmail.com>
2021-03-12 19:59:49 +0200

    possible fix for #1

--- src/classes/Settings.ts
+++ src/classes/Settings.ts
@@ -3,8 +3,10 @@ export class Settings {
 	singleServerMode: boolean;
 	autosaveDelay: number;
     clientDownloadLink: string;
+    exitOnPromiseReject: boolean;
 	constructor() {
 		this.port = "8877";
+		this.exitOnPromiseReject = false;
 		this.autosaveDelay = 5;
 		this.singleServerMode = false;
 		this.clientDownloadLink = "https://github.com/20PercentRendered/servcord-client/releases/latest";
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { Logger } from "./logger";
 import { ServerData } from "./serverdata";
-
+// app sometimes exits without an exit code, this is a "bodge" to temp fix things
+process.on('uncaughtException', function (err) {
+    console.error(err);
+    process.exit(1);
+})
 // Load everything
 ServerData.getInstance();
+process.on('unhandledRejection', function(reason, promise){
+    console.error(reason)
+    if (ServerData.getInstance().settings.exitOnPromiseReject) {
+        process.exit(1);
+    }
+});
 var logger = new Logger("Init");
 logger.info("Begin startup sequence.");
 require("./loader").init();
